1f4a92f90af7fbd1724f496629cbfef0bdcbdfcf
ethan <graf.ethan@gmail.com>
2020-05-01 19:34:22 -0400

    Basic schema loading

--- code/load_data.py
+++ code/load_data.py
@@ -1,4 +1,6 @@
 import psycopg2
+import psycopg2.extras
+import csv
 
 connection_string = "host='localhost' dbname='dbms_final_project' user='dbms_project_user' password='dbms_password'"
 
@@ -8,6 +10,29 @@ connection_string = "host='localhost' dbname='dbms_final_project' user='dbms_pro
 def main():
     # TODO invoke your code to load the data into the database
     print("Loading data")
+
+    # Get db connection
+    conn = psycopg2.connect(connection_string)
+
+    # Read in input data
+    with conn.cursor() as cursor:
+        # Load schema
+        with open('schema.sql', 'r') as schema:
+            setup_queries = schema.read()
+            cursor.execute(setup_queries)
+        conn.commit()
+
+        # Load zip data
+        with open('datasets/uszips.csv', 'r') as zip_data:
+            csv_reader = csv.reader(zip_data, delimiter=',')
+
+            # Print a single row
+            count = 0
+            for row in csv_reader:
+                count += 1
+            print(count)
+
+
 
 
 if __name__ == "__main__":
--- code/schema.sql
+++ code/schema.sql
@@ -1,4 +1,7 @@
-CREATE TABLE test(
+DROP SCHEMA IF EXISTS db_project CASCADE;
+CREATE SCHEMA db_project;
+
+CREATE TABLE db_project.test(
     a VARCHAR(15),
     b INT
 );
