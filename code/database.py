b13d2470f703642664ca5aa9adf53abcd8e9d7ab
devrangarajan <rangad@rpi.edu>
2020-05-03 13:56:59 -0700

    Laid out queries

--- code/database.py
+++ code/database.py
@@ -13,13 +13,34 @@ class database:
 
     # Get % of essential businesses
     def getEssentialDensity(self, zipCode):
+        #Get total # of businesses in zip
+        #Get total # of essential businesses in that list (use a sub query)
+        #Return essential/total
         return None
     
     # Show change in essential density after changing essential list
     def getEssentialDensityDelta(self, zipCode):
+        #Get essential density before change, get density after change
+        return None
+
+    def getMedianIncome(self, zipCode):
+        return None
+    
+    def getMedianAge(self, zipCode):
+        return None
+
+    # Get essential % for all of nyc
+    def getTotalEssential(self):
         return None
 
     def getIndustriesInZipCode(self, zipCode):
         q = "SELECT Industry FROM db_project.business WHERE address_zip = %s GROUP BY Industry"
         return self.query(q, zipCode)
 
+    # TODO make a python list of essential industries that's modifiable
+    def addEssentialBusiness(self, newBiz):
+        #Check that newBiz is in the valid industry
+        return None
+
+    def removeEssentialBusiness(self, remBiz):
+        return None   
