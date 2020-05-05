94a5ed6b3ecb7ae9155c70c401de8c5934cc7d60
saidmf <saidmf@rpi.edu>
2020-05-05 00:49:21 -0400

    Combined density and count into one function

--- visualization/essentialData.py
+++ visualization/essentialData.py
@@ -40,64 +40,40 @@ class essentialData:
         query = "SELECT address_zip FROM db_project.business WHERE essential=TRUE"
         cursor.execute(query)
         essential = cursor.fetchall()
-        cursor.close() 
 
         print("Loading essential zips into db...")
 
-        df = pd.DataFrame(essential, columns=['zip']).dropna() 
-        dupes = df.pivot_table(index=['zip'], aggfunc='size')
+        dfCount = pd.DataFrame(essential, columns=['zip']).dropna() 
+        dupes = dfCount.pivot_table(index=['zip'], aggfunc='size')
 
-        df.sort_values('zip', inplace=True)
-        df.drop_duplicates(subset=None, keep='first', inplace=True)
+        dfCount.sort_values('zip', inplace=True)
+        dfCount.drop_duplicates(subset=None, keep='first', inplace=True)
 
-        df['essential'] = np.asarray(dupes)
-        df['zip'] = df['zip'].astype(str)
+        dfCount['essential'] = np.asarray(dupes)
+        dfCount['zip'] = dfCount['zip'].astype(str)
 
-        print("Creating ny choro map...")
+        print("Creating ny choro count map...")
 
-        countMap = folium.Choropleth(geo_data='nyczip.geojson', data=df, columns=['zip', 'essential'], \
+        countMap = folium.Choropleth(geo_data='nyczip.geojson', data=dfCount, columns=['zip', 'essential'], \
                         key_on='feature.properties.postalCode', fill_color='OrRd', fill_opacity=.7, \
                         legend_name='Essentials Per Zip', show=False)
         map.add_child(countMap)
-
-    def create_essentialDensity_Map(self, map):
-        cursor = self.conn.cursor()
-        cursor.execute("ALTER TABLE db_project.business  \
-            DROP COLUMN IF EXISTS essential;")
-        cursor.execute("ALTER TABLE db_project.business \
-            ADD COLUMN essential boolean NOT NULL DEFAULT FALSE;")
-
-        print("Getting essential businesses and creating column...")
-
-        essential_Businesses = ["Booting Company", "Process Server Individual", "Tow Truck Driver", 
-        "Garage", "Laundry", "Employment Agency", "Tow Truck Company", "Tow Truck Exemption", "Laundries",
-        "Stoop Line Stand", "Debt Collection Agency", "Newsstand", "Garage and Parking Lot", "Laundry Jobber",
-        "Parking Lot", "Process Serving Agency", "Storage Warehouse"]
-
-        for business in essential_Businesses:
-            query = "UPDATE db_project.business \
-                SET essential=TRUE  \
-                WHERE industry=%s"
-            cursor.execute(query, (business,))
-        self.conn.commit() 
 
         query = "SELECT address_zip, essential FROM db_project.business"
         cursor.execute(query)
         essential = cursor.fetchall()
         cursor.close() 
 
-        print("Loading essential zips into db...")
-
-        df = pd.DataFrame(essential, columns=['zip', 'essential']).dropna() 
-        df = df.groupby(['zip', 'essential']).size().reset_index(name='Count')
+        dfDense = pd.DataFrame(essential, columns=['zip', 'essential']).dropna() 
+        dfDense = dfDense.groupby(['zip', 'essential']).size().reset_index(name='Count')
     
         # Create placeholder column for df density
-        df['essentialDensity'] = np.nan
+        dfDense['essentialDensity'] = np.nan
 
-        for index, row in df.iterrows():
+        for index, row in dfDense.iterrows():
             zip_code = row["zip"]
-            trueCount = (df[(df['essential'] == True) & (df['zip'] == zip_code)])["Count"]
-            falseCount = (df[(df['essential'] == False) & (df['zip'] == zip_code)])["Count"]
+            trueCount = (dfDense[(dfDense['essential'] == True) & (dfDense['zip'] == zip_code)])["Count"]
+            falseCount = (dfDense[(dfDense['essential'] == False) & (dfDense['zip'] == zip_code)])["Count"]
 
             if(len(trueCount) == 0):
                 trueCount = 0
@@ -110,15 +86,15 @@ class essentialData:
                 falseCount = falseCount.item()
 
             density = float(trueCount)/(float(trueCount) + float(falseCount))
-            df.at[index, "essentialDensity"] = density
+            dfDense.at[index, "essentialDensity"] = density
 
-        df['zip'] = df['zip'].astype(str)
-        df = df.drop(labels=['Count', 'essential'], axis=1)
-        df.drop_duplicates(subset='zip', keep='first', inplace=True)
+        dfDense['zip'] = dfDense['zip'].astype(str)
+        dfDense = dfDense.drop(labels=['Count', 'essential'], axis=1)
+        dfDense.drop_duplicates(subset='zip', keep='first', inplace=True)
 
-        print("Creating ny choro map...")
+        print("Creating ny choro dense map...")
 
-        densityMap = folium.Choropleth(geo_data='nyczip.geojson', data=df, columns=['zip', 'essentialDensity'], \
+        densityMap = folium.Choropleth(geo_data='nyczip.geojson', data=dfDense, columns=['zip', 'essentialDensity'], \
                         key_on='feature.properties.postalCode', fill_color='OrRd', fill_opacity=.7, \
                         legend_name='Essentials Per Zip', show=False)
         map.add_child(densityMap)
