9c1639bf9ca754cc9ff81208c821aaad1a71df16
Gary Wang <38331932+gwang111@users.noreply.github.com>
2020-05-05 23:44:52 -0700

    Update application.py

--- code/application.py
+++ code/application.py
@@ -1,21 +1,27 @@
 import folium
+import database as db
 import essentialData as ess
 
 # Define our variables
 nyMap = folium.Map(location=[40.7128, -74.0060], titles='Stamen Toner', zoom_start=11)
-mapDisp = ess.essentialData()
+mapObj = ess.essentialData()
+dbObj = db.database()
 
+print('\nRunning Database Systems Final Project...')
+print("\n-Enter 'info' for a list of our features and how to use them")
+print("-Enter 'exit' to Exit\n")
 while(1):
-    print("Running Database Systems Final Project...")
-    print("Enter 'info' for a list of our features and how to use them")
-    print("Enter 'quit' to Exit")
-    val = input("Type a Command: ")
-    if val == 'quit':
-        print("Exiting Program...")
+    val = input('Type a Command: ')
+    if val == 'exit':
+        print('\nExiting Program...\n')
         break
-    if val == 'display':
-        mapDisp.show(nyMap)
-    if val == 'add'
-      #...
-    if val == #...
-      #...
+    elif val == 'display':
+        print('\nDisplaying Map...\n')
+        mapObj.show(nyMap)
+    elif val == 'add':
+        print('Enter a list of your essential businesses: ')
+    elif val == 'query':
+        print('querying')
+    else:
+        print('\nInvalid Input\n')
+    
