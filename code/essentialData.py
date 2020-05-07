814b35fa2ae613520eb3c7975248c1ce1e245b2c
saidmf <saidmf@rpi.edu>
2020-05-06 22:55:08 -0400

    Documentation

--- code/essentialData.py
+++ code/essentialData.py
@@ -107,20 +107,24 @@ class essentialData:
     # Generate a dataframe with a zips column and a result column that is the "func" applied to all
     # the zips in NYC 
     def generateZips(self, func):
+        # Connect to db and get zips of nyc 
         cursor = self.conn.cursor()
         query = "SELECT address_zip FROM db_project.business"
         cursor.execute(query)
         zips = cursor.fetchall()
 
+        # Create df w/ zip column and empty result column 
         df = pd.DataFrame(essential, columns=['zip']).dropna() 
         df.drop_duplicates(subset='zip', keep='first', inplace=True)
         df['result'] = 0
 
+        # Apply func to each zip code 
         for index, row in df.iterrows():
             zip_code = row["zip"]
             result = func(zip_code)
             df.at[index, "result"] = result
 
+        # Pass completed df to essential_map which creates layer, and then show, which presents map
         nyMap = folium.Map(location=[40.7128, -74.0060], titles='Stamen Toner', zoom_start=11)
         self.essential_map(nyMap, func.__qualname__, func.__qualname__, df)
         self.show(nyMap)
